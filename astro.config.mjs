9e617a740dce093ead3c9b9e6337e6a73ce8aabf
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-10-24 14:54:20 +0700

    bump actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-const site = process.env.site || "http://localhost:4321";
-const base = process.env.base || "/";
+const site = process.env.SITE || "http://localhost:4321";
+const base = process.env.BASE || "/";
 
 // https://astro.build/config
 export default defineConfig({
